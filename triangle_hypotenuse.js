906215f406ae82fe503aba21bef0abf9a1eeeef2
Vidhi Makani <100896986+vidhimakani74@users.noreply.github.com>
2022-11-12 17:02:49 +0530

    find hypotenuse added

---
+++ triangle_hypotenuse.js
@@ -0,0 +1,20 @@
+const length = document.querySelectorAll(".length");
+const calculateBtn = document.querySelector(".calculate-btn");
+const lengthHypotenuse = document.querySelector(".result");
+
+let hypotenuse;
+function calculateHypotenuse() {
+    // var firstLength = Number(length[0].value);
+    // var secondLength = Number(length[1].value);
+
+    if (Number(length[1].value) > 0 && Number(length[1].value) > 0) {
+        hypotenuse = Math.sqrt(Number(length[0].value) ** 2 + Number(length[1].value) ** 2);
+        lengthHypotenuse.innerText = "Length of Hypotenuse is: " + hypotenuse + "cm";
+        lengthHypotenuse.style.color = "green";
+    }
+    else {
+        lengthHypotenuse.innerText = "Please enter values of sides above zero!";
+        lengthHypotenuse.style.color = "red";
+    }
+}
+calculateBtn.addEventListener("click", calculateHypotenuse);
