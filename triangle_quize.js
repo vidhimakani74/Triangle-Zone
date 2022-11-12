0519b821bb3645439db8b9764f3a1e1a6099f8c6
Vidhi Makani <100896986+vidhimakani74@users.noreply.github.com>
2022-11-12 16:35:12 +0530

    quiz added

--- triangle_quize.js
+++ triangle_quize.js
@@ -1,29 +1,20 @@
-var formQuize=document.querySelector(".quize-form");
-var submitBtn=document.querySelector(".submit-btn");
-var output=document.querySelector(".output");
+var formQuize = document.querySelector(".quize-form");
+var submitBtn = document.querySelector(".submit-btn");
+var output = document.querySelector(".output");
 
-const answers=["55°","Isosceles","4 cm","BF = CE","60°"]
+const answers = ['55°', "Isosceles", " 4 cm", "BF = CE"];
 
-function calculateScore(){
-    
-    let score=0;
-    let index=0;
-    const formAns=new FormData(formQuize);
-    for(let value in formAns.values()){
-        if(value===answers[index]){
-            score=score+1;
+function calculateScore() {
+    let score = 0;
+    let index = 0;
+    const formResult = new FormData(formQuize);
+    for (let value of formResult.values()) {
+        if (value === answers[index]) {
+            score = score + 1;
         }
-            index=index+1;
-        }
-    if(score>2){
-        output.innerText="Passed! Your Score is: " + score + "/5 ";
-        output.style.color = "green";
+        index = index + 1;
     }
-    else{
-        output.innerText="Failed!Your Score is: " + score + "/5.";
-        output.style.color = "red";
-    }
+    output.innerText = "your score is " + score + "/4"
 }
 
-
-submitBtn.addEventListener("click",calculateScore);
+submitBtn.addEventListener("click", calculateScore);
